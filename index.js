03e24b6ad44a5ad83010a53e43340e8ac5b0dff6
Sylvestre Gug <sylvestre.gug@gmail.com>
2017-01-28 13:35:53 -0500

    Twitter socket builded

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
 var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Twit = require('twit');
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/dist'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/dist');
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
 
-app.get('/', function(request, response) {
-  response.render('index.html');
+app.get('/', function(req, res){
+	res.render('index.html');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+io.on('connection', function(socket){
+	console.log('a user connected');
+
+	// On new connection, start a Stream.
+	var T = new Twit({
+		consumer_key:         'GJuIZGB3vgUO4ARU44N8rLBsb',
+		consumer_secret:      'RBiPbwyBSx4WhhnNtyCZvRX31r9jLnGiGcqzmXkclDh81Kris0',
+		access_token:         '90852060-LFiIvufx6T5xDuYIH13hwEGaICwvkr15Dky2km2PC',
+		access_token_secret:  'jgcZTGavy0EJYZmCEJSKlgPiTqwSmM3xDTNxAbNlSzoB0',
+		timeout_ms:           60*1000,
+	});
+	var stream = T.stream('statuses/sample');
+
+	// On each tweet, emit a tweet event to the socket.
+	stream.on('tweet', function (tweet) {
+		io.emit('tweet', tweet);
+	});
+
+	stream.on('limit', function (msg) {
+		io.emit('limit', msg);
+	});
+
+	//On disconnect, destroy the stream.
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+});
+
+/*app.get('/twit/', function(request, response){
+	var _response = response;
+	var _request = request;
+	var T = new Twit({
+		consumer_key:         'GJuIZGB3vgUO4ARU44N8rLBsb',
+		consumer_secret:      'RBiPbwyBSx4WhhnNtyCZvRX31r9jLnGiGcqzmXkclDh81Kris0',
+		access_token:         '90852060-LFiIvufx6T5xDuYIH13hwEGaICwvkr15Dky2km2PC',
+		access_token_secret:  'jgcZTGavy0EJYZmCEJSKlgPiTqwSmM3xDTNxAbNlSzoB0',
+		timeout_ms:           60*1000,
+	});
+
+	var stream = T.stream('statuses/sample');
+
+	stream.on('tweet', function (tweet) {
+		console.log(tweet.text);
+		io.emit('tweet',tweet);
+	});
+
+});*/
+
+http.listen(5000, function(){
+	console.log('listening on *:5000');
 });
